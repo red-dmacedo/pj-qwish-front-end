ebb13e3367a7f9c497021a1d4f8d8e971baac898
JamesYon <trickymoon01@gmail.com>
2025-08-25 15:55:14 -0400

    finished UserContext

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { Routes, Route } from "react-router";
+import { useContext, useState, useEffect } from "react";
+
+import NavBar from "./components/NavBar/NavBar";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignInForm from './components/SignInForm/SignInForm';
+import QwishList from './components/QwishList/QwishList';
+
+import { UserContext } from "./contexts/UserContext";
+
+import * as qwishService from './services/qwishService';
+
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [lists, setLists] = useState([]);
+
+  const { user } = useContext(UserContext);
+
+  function handleLogOut() {
+    setAuthenticated(false);
+    setLists([]);
+    localStorage.removeItem("token");
+    //we need to clear lists, tokens
+  }
+
+useEffect(() => {
+  const fetchAllLists = async () => {
+    const listsData = await qwishService.index();
+
+    setLists(listsData);
+  };
+
+  if (user) fetchAllLists();
+}, [user]);
+
+  return (
+    <>
+      <NavBar authenticated={authenticated} handleLogOut={handleLogOut} />
+      <Routes>
+        <Route index element={<Home />} />
+
+        <Route
+          path="sign-in"
+          element={<SignInForm setAuthenticated={setAuthenticated} />}
+        />
+        <Route
+          path="sign-up"
+          element={<SignUpForm setAuthenticated={setAuthenticated} />}
+        />
+        <Route path="/lists" element={<QwishList lists={lists} />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
