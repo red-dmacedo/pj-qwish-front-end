2b36108db763161b2d1b242dcafcb136bf02974a
Sharon Lee <ssmlee10@gmail.com>
2025-08-25 11:46:57 -0700

    cleanup component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
 import { Routes, Route } from "react-router";
-import { useState,useEffect } from "react";
-import Home from "./components/pages/Home";
-import Nav from "./components/Nav";
-import SignIn from "./components/pages/SignInForm/SignInForm";
-import SignUp from "./components/pages/SignUpForm";
-import Lists from "./components/pages/Lists";
-import NewList from "./components/pages/NewList";
-import ListItem from "./components/pages/ListItem";
-import Tools from "./components/pages/Tools";
-import { getLists } from "./api/controller";
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar/NavBar";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignInForm from './components/SignUpForm/SignUpForm';
 
 const App = () => {
   const [authenticated, setAuthenticated] = useState(false);
-  const [lists, setLists] = useState([])
+  const [lists, setLists] = useState([]);
 
   function handleLogOut() {
     setAuthenticated(false);
-    setLists([])
-    localStorage.removeItem('token')
+    setLists([]);
+    localStorage.removeItem("token");
     //we need to clear lists, tokens
   }
 
-  useEffect(()=>{
-    if(authenticated){
-      getLists().then(setLists)
-    }else{
-      const token = localStorage.getItem('token')
-      if(token){
-        setAuthenticated(token)
+  useEffect(() => {
+    if (authenticated) {
+      getLists().then(setLists);
+    } else {
+      const token = localStorage.getItem("token");
+      if (token) {
+        setAuthenticated(token);
       }
     }
-  },[authenticated])
+  }, [authenticated]);
 
   return (
     <>
-      <Nav authenticated={authenticated} handleLogOut={handleLogOut} />
+      <NavBar authenticated={authenticated} handleLogOut={handleLogOut} />
       <Routes>
         <Route index element={<Home />} />
 
-        <Route path="sign-in" element={<SignIn setAuthenticated={setAuthenticated}/>} />
-        <Route path="sign-up" element={<SignUp setAuthenticated={setAuthenticated}/>} />
-        <Route path="lists" element={<Lists lists={lists}/>} />
+        <Route
+          path="sign-in"
+          element={<SignInForm setAuthenticated={setAuthenticated} />}
+        />
+        <Route
+          path="sign-up"
+          element={<SignUpForm setAuthenticated={setAuthenticated} />}
+        />
+        <Route path="lists" element={<Lists lists={lists} />} />
         <Route path="lists/new-list" element={<NewList />} />
         <Route path="lists/:id" element={<ListItem />} />
         <Route path="tools" element={<Tools />} />
