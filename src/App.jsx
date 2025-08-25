c75fe67a6b84d86b929b669a0814e7a69a4aa79f
Sharon Lee <ssmlee10@gmail.com>
2025-08-25 12:08:19 -0700

    create QwishList component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import SignUpForm from "./components/SignUpForm/SignUpForm";
 import SignInForm from './components/SignInForm/SignInForm';
+import QwishList from './components/QwishList/QwishList';
 
 const App = () => {
   const [authenticated, setAuthenticated] = useState(false);
@@ -40,7 +41,7 @@ const App = () => {
           path="sign-up"
           element={<SignUpForm setAuthenticated={setAuthenticated} />}
         />
-        <Route path="lists" element={<Lists lists={lists} />} />
+        <Route path="/lists" element={<QwishList lists={lists} />} />
         <Route path="lists/new-list" element={<NewList />} />
         <Route path="lists/:id" element={<ListItem />} />
         <Route path="tools" element={<Tools />} />
