f4013b5e83680233820d0b1b9937bfe898f1dfb7
Sharon Lee <ssmlee10@gmail.com>
2025-08-25 14:22:22 -0700

    create QwishForm component, update routes in App.jsx, create AddList, UpdateList, DeleteList functions

---
+++ src/components/QwishForm/QwishForm.jsx
@@ -0,0 +1,16 @@
+// import { useState, useEffect } from 'react';
+// import { useParams } from "react-router";
+
+// import * as qwishService from "../../services/qwishService";
+
+const QwishForm = () => {
+    // const { qwishId } = useParams();
+
+    return (
+        <main>
+            <h1>Qwish Form</h1>
+        </main>
+    )
+}
+
+export default QwishForm;
