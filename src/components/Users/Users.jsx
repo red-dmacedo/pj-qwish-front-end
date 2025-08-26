4de59b4f8053e6555dc81470c46879990a065118
devin <devinm3333@outlook.com>
2025-08-26 08:56:40 -0700

    removed un-used useState variable

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router';
 import { UserContext } from '../../contexts/UserContext';
 
 const Users = (props) => {
-  const { newFriend, setNewFriend } = useState('');
   const { user } = useContext(UserContext);
   const sortedUsers = props.users.toSorted((a, b)=> a.name.localeCompare(b.name));
   const { filteredUsers, setFilteredUsers } = useState(sortedUsers);
