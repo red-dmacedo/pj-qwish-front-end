329678c0c07d14262d350264b5654b7c541826e7
JamesYon <trickymoon01@gmail.com>
2025-08-27 10:39:24 -0400

    added UI

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,13 +1,10 @@
-// src/components/Landing.jsx
-
 const Landing = () => {
   return (
     <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
+      <h1>Qwish</h1>
       <p>Sign up now, or sign in to see your super secret dashboard!</p>
     </main>
   );
 };
 
 export default Landing;
-
--- src/components/QwishList/QwishList.jsx
+++ src/components/QwishList/QwishList.jsx
@@ -1,11 +1,21 @@
 // import { Link } from 'react-router';
 
-const QwishList = () => {
-    return (
-        <main>
-            <h1>Qwishlists</h1>
-        </main>
-    )
-}
+import { Link } from "react-router";
+
+const QwishList = (props) => {
+  return (
+    <main>
+    <h1>Qwishlists</h1>
+      {props.lists.map((list) => (
+        <article key={list._id}>
+          <header>
+            <p>{list.author.username}</p>
+          </header>
+          <Link key={list._id} to={`/lists/${list._id}`}><p>{list.name}</p></Link>
+        </article>
+      ))}
+    </main>
+  );
+};
 
 export default QwishList;
