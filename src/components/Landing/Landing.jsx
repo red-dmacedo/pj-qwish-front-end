4deeae17ab42ef481ce22ef6a702b90899717b01
Sharon Lee <ssmlee10@gmail.com>
2025-08-28 07:50:26 -0700

    update landing page text about qwish, benefits, how it works, newsletter signup

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,7 +4,31 @@ const Landing = () => {
   return (
     <main className={styles.container}>
       <h1>Qwish</h1>
-      <p>Sign up now, or sign in to see your super secret dashboard!</p>
+      <h2>Making gift-giving easy, fun, and stress-free.</h2>
+
+      <h2>Why use it?</h2>
+      <p><b>No more awkward gifts</b><br />
+      Share exactly what you want. No more guessing needed!</p>
+
+      <p><b>Save time</b><br />
+      Friends and family will know exactly what to buy.</p>
+
+      <p><b>Collaborate on lists</b>
+      <br />Add items together for weddings, holidays, or birthdays.</p>
+
+      <p><b>No more returns</b><br />
+      Get gifts you'll actually keep and love.</p>
+      <p><b>Better for the environment</b><br />
+      Reduce waste from unwanted gifts.</p>
+
+      <h2>How it Works</h2>
+      <p>(3 step visual with screenshots)</p>
+      <p>1. Create your wishlist <br />
+      2. Add items from anywhere <br />
+      3. Share with friends and family to purchase.</p>
+
+      <h2>Newsletter Signup</h2>
+      <p>(Newsletter signup form)</p>
     </main>
   );
 };
