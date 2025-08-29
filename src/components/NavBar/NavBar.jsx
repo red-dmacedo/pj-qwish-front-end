a8e5ff62c7223c3d8a94f5f57f7a9fe7e6c1f6e8
Sharon Lee <ssmlee10@gmail.com>
2025-08-29 09:45:21 -0700

    remove home from navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -17,7 +17,6 @@ const NavBar = () => {
         <Link to='/' className={styles.dynapuff}>Qwish</Link>
         { user ? (
             <ul>
-            <li><Link to='/'>Home</Link></li>
             <li><Link to='/lists'>My Lists</Link></li>
             <li><Link to='/lists/new'>New List</Link></li>
             <li><Link to='/users'>Find Friends</Link></li>
