a077ea26adfee71d5910f1965df8e03e474fa6e6
devin <devinm3333@outlook.com>
2025-08-29 08:30:19 -0700

    pre-merge

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,33 +3,37 @@ import styles from "./Landing.module.scss";
 const Landing = () => {
   return (
     <main className={styles.container}>
-    <div className={styles.bodyText}>
-      <h1>Qwish</h1>
-      <h2>Making gift-giving easy, fun, and stress-free.</h2>
+      <div className={styles.bodyText}>
+        <h1>Qwish</h1>
+        <h2>Making gift-giving easy, fun, and stress-free.</h2>
+        <h2>Why use it?</h2>
 
-      <h2>Why use it?</h2>
-      <p><b>No more awkward gifts</b><br />
-      Share exactly what you want. No more guessing needed!</p>
+        <h4>No more awkward gifts</h4>
+        <p>Share exactly what you want. No more guessing needed!</p>
 
-      <p><b>Save time</b><br />
-      Friends and family will know exactly what to buy.</p>
+        <h4>Save Time</h4>
+        <p>Friends and family will know exactly what to buy.</p>
 
-      <p><b>Collaborate on lists</b>
-      <br />Add items together for weddings, holidays, or birthdays.</p>
+        <h4>Collaborate on lists</h4>
+        <p>Add items together for weddings, holidays, or birthdays.</p>
 
-      <p><b>No more returns</b><br />
-      Get gifts you'll actually keep and love.</p>
-      <p><b>Better for the environment</b><br />
-      Reduce waste from unwanted gifts.</p>
+        <h4>No more returns</h4>
+        <p>Get gifts you'll actually keep and love.</p>
 
-      <h2>How it Works</h2>
-      <p>(3 step visual with screenshots)</p>
-      <p>1. Create your wishlist <br />
-      2. Add items from anywhere <br />
-      3. Share with friends and family to purchase.</p>
+        <h4>Better for the environment</h4>
+        <p>Reduce waste from unwanted gifts.</p>
 
-      <h2>Newsletter Signup</h2>
-      <p>(Newsletter signup form)</p>
+        <h2>How it Works</h2>
+        <ol>
+          <p>(3 step visual with screenshots)</p>
+          <li>Create your wishlist</li>
+          <li>Add items from anywhere</li>
+          <li>Share with friends and family to purchase.</li>
+        </ol>
+
+        {/* Wait to show the Newsletter until it it complete */}
+        {/* <h2>Newsletter Signup</h2>
+        <p>(Newsletter signup form)</p> */}
       </div>
     </main>
   );
