6e3c702d9fa247d1c7bc2467bd548ee212b7cd96
Sharon Lee <ssmlee10@gmail.com>
2025-08-29 14:47:26 -0700

    add qwishlist card details

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import AppStore from "../../assets/images/downloadApp.png";
+
+const Footer = () => {
+  <>
+  <h1>Footer</h1>
+    <div>
+      <img src={AppStore} alt="download our app from the app store" />
+    </div>
+  </>;
+};
+
+export default Footer;
