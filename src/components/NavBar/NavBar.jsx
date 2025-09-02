06283a2a77f30ce6ad9e8a36ff524a363265731f
devin <devinm3333@outlook.com>
2025-09-02 10:41:35 -0700

    navbar and landing page updated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,33 +5,33 @@ import styles from "./NavBar.module.scss";
 import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
-    const location = useLocation()
-    const { user, setUser } = useContext(UserContext);
+  const location = useLocation()
+  const { user, setUser } = useContext(UserContext);
 
-    const handleSignOut = () => {
-        localStorage.removeItem('token');
-        setUser(null);
-    };
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
 
-    return (
-        <nav className={styles.container}>
-        <Link to='/' className={styles.dynapuff}>Qwish</Link>
-        { user ? (
-            <ul>
-            <li><Link to='/lists'>My Lists</Link></li>
-            <li><Link to='/lists/new'>New List</Link></li>
-            <li><Link to='/users'>Find Friends</Link></li>
-            <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
-            </ul>
-        ) : location.pathname == "/" && (
-            <ul>
-                <li><Link to='/sign-in'>Sign In</Link></li>
-                <li><Link to='/sign-up'>Sign Up</Link></li>
-            </ul>
-        )}
-        
-        </nav>
-    )
+  return (
+    <nav className={styles.container}>
+      <Link to='/' className={styles.dynapuff}>Qwish</Link>
+      {user ? (
+        <ul>
+          <li><Link to='/lists'>My Lists</Link></li>
+          <li><Link to='/lists/new'>New List</Link></li>
+          <li><Link to='/users'>Find Friends</Link></li>
+          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+        </ul>
+      ) : location.pathname == "/" && (
+        <ul>
+          <li><Link to='/sign-in'>Sign In</Link></li>
+          <li><Link to='/sign-up'>Sign Up</Link></li>
+        </ul>
+      )}
+
+    </nav>
+  )
 }
 
 export default NavBar;
